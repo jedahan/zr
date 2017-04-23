7b260549e34f7c73f7ca2ccde6e7a8d7399b37c5
Jonathan Dahan <hi@jonathan.is>
2017-04-23 15:11:14 -0400

    We have correct path logic now

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::{env,fmt};
-use std::path::Path;
+use std::path::PathBuf;
 
 const VERSION: &'static str = "0.0.1";
 
-struct Plugin<'a> {
-    repo: &'a Path,
-    files: Vec<&'a Path>
+#[derive(Debug)]
+struct Plugin {
+    repo: PathBuf,
+    files: Vec<PathBuf>
 }
 
-impl<'a> fmt::Display for Plugin<'a> {
+impl fmt::Display for Plugin {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let prefix = self.repo.display();
-        for file in self.files {
+        for file in &self.files {
             let basedir = file.parent().unwrap().display();
             write!(f, r"source {}/{}", prefix, file.display())?;
             write!(f, r"fpath+={}/{}/", prefix, basedir)?;
@@ -21,23 +22,23 @@ impl<'a> fmt::Display for Plugin<'a> {
     }
 }
 
-impl<'a> Plugin<'a> {
-    pub fn from_path(path: &'a Path) -> Plugin<'a> {
-        let name = path.iter().last().unwrap();
-        let mut files = path.read_dir().unwrap().filter_map(std::result::Result::ok).map(|file| file.path().as_path());
+impl Plugin {
+    pub fn from_path(path: PathBuf) -> Plugin {
+        let path_clone = path.clone();
+        let name = path_clone.iter().last().unwrap();
+        let files: Vec<_> = path.read_dir().unwrap().filter_map(std::result::Result::ok).map(|file| file.path()).filter(|file| file.is_file()).collect();
 
-        // antigen style plugins
-        if files.any(|file| *file == path.join(name).join(".plugin.zsh")) {
-            let _files = files.filter(|file| *file == path.join(name).join(".plugin.zsh")).collect();
+        let antigen_plugin_file = files.iter().find(|file| file.file_name().unwrap() == path.join(name).join(".plugin.zsh"));
+        if antigen_plugin_file.is_some() {
             return Plugin {
                 repo: path,
-                files: _files
+                files: vec![antigen_plugin_file.unwrap().to_owned()]
             }
         }
 
-        // prezto style plugins
-        if files.any(|file| *file == *path.join("init.zsh")) {
-            let _files = files.filter(|file| *file == path.join("init.zsh")).collect();
+        // prezto: if we find init.zsh, try to load with pmodload, or manually
+        let prezto_plugin_file = files.iter().find(|file| file.file_name().unwrap() == path.join("init.zsh"));
+        if prezto_plugin_file.is_some() {
             return match std::process::Command::new("pmodload").arg(name.clone()).spawn() {
                 Ok(_) =>
                     Plugin {
@@ -47,24 +48,26 @@ impl<'a> Plugin<'a> {
                 Err(_) =>
                     Plugin {
                         repo: path,
-                        files: _files
+                        files: vec![prezto_plugin_file.unwrap().to_owned()]
                     }
             }
         }
 
         // zsh plugins
-        if files.any(|file| file.ends_with(".zsh")) {
+        let zsh_plugin_files: Vec<_> = files.iter().filter(|file| file.extension().unwrap() == "zsh").map(|e| e.to_owned()).collect();
+        if ! zsh_plugin_files.is_empty() {
             return Plugin {
                 repo: path,
-                files: files.filter(|file| file.ends_with(".zsh")).collect(),
+                files: zsh_plugin_files
             }
         }
 
         // sh plugins
-        if files.any(|file| file.ends_with(".sh")) {
+        let sh_plugin_files: Vec<_> = files.iter().filter(|file| file.extension().unwrap() == "sh").map(|e| e.to_owned()).collect();
+        if ! sh_plugin_files.is_empty() {
             return Plugin {
                 repo: path,
-                files: files.filter(|file| file.ends_with(".sh")).collect(),
+                files: sh_plugin_files.to_vec()
             }
         }
 
@@ -74,14 +77,14 @@ impl<'a> Plugin<'a> {
 
 fn main() {
     let default_home = format!("{}/.zr", env!("HOME"));
-    let zr_home = Path::new(option_env!("ZR_HOME").unwrap_or(default_home.as_str()));
+    let zr_home = PathBuf::from(option_env!("ZR_HOME").unwrap_or(default_home.as_str()));
 
     match env::args().nth(1) {
         Some(command) => {
             match command.as_ref() {
                 "version" => version(),
-                "debug" => debug(&zr_home),
-                "load" => load(&zr_home, &Path::new(&env::args().nth(2).unwrap())),
+                "debug" => debug(zr_home),
+                "load" => load(zr_home, PathBuf::from(env::args().nth(2).unwrap())),
                 _ => help(),
             }
         },
@@ -89,7 +92,7 @@ fn main() {
     };
 }
 
-fn debug(zr_home: &Path) {
+fn debug(zr_home: PathBuf) {
     version();
     println!("  ZR_HOME: {}", zr_home.display());
 }
@@ -113,10 +116,12 @@ fn version() {
     println!("{}", VERSION);
 }
 
-fn load(zr_home: &Path, name: &Path) {
+fn load(zr_home: PathBuf, name: PathBuf) {
     println!("loading {:?}", name.display());
-    let plugin_path = format!("{}/{}", zr_home.display(), name.display());
-    let plugin = Plugin::from_path(Path::new(&plugin_path));
+    let plugin_path = format!("{}/plugins/{}", zr_home.display(), name.display());
+    println!("from path {:?}", plugin_path);
+    let plugin = Plugin::from_path(PathBuf::from(&plugin_path));
     println!("loaded {:?}", name.display());
     println!("{}", plugin);
+    println!("{:?}", plugin);
 }
