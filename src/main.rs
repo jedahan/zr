505c83198846caba00c0e1b98d3ab9ce0d8b79a1
Jonathan Dahan <hi@jonathan.is>
2017-05-13 15:53:02 -0400

    split plugin id from single string to author and name

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,15 @@ extern crate clap;
 extern crate git2;
 extern crate libc;
 
-/// The prelude module makes it easy to split this file into multiple files
-mod prelude {
-    pub use std::fmt;
-    pub use std::path::{Path, PathBuf};
-    pub use std::fs;
-    pub use std::io::{BufRead, BufReader, ErrorKind, Write};
-    pub use std::collections::HashSet;
-    pub use std::fs::OpenOptions;
-    pub use std::ffi::{OsStr,OsString};
-    pub use git2::Repository;
-}
-
-use prelude::*;
+use std::fmt;
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+use std::collections::HashSet;
+use std::fs::OpenOptions;
+use std::ffi::{OsStr,OsString};
+use git2::Repository;
+use std::iter::FromIterator;
 
 /// eprintln! will be in stable soon
 macro_rules! eprintln {
@@ -32,14 +28,15 @@ macro_rules! eprintln {
     }}
 }
 
-/// A plugin is just a simple name (like "geometry"), and a list of files to load
 struct Plugin {
+    author: String,
     name: String,
     files: HashSet<PathBuf>
 }
 
 enum Error {
-    EnvironmentVariableNotUnicode { key: String, value: OsString }
+    EnvironmentVariableNotUnicode { key: String, value: OsString },
+    InvalidPluginName { plugin_name: String },
 }
 
 impl fmt::Display for Error {
@@ -48,6 +45,8 @@ impl fmt::Display for Error {
         match *self {
             EnvironmentVariableNotUnicode {ref key, ref value} =>
                 write!(f, "The value in the environment variable '{}' is not utf-8: {}", key, value.to_string_lossy()),
+            InvalidPluginName {ref plugin_name} =>
+                write!(f, "The plugin name must be formatted 'author/name', found '{}'", plugin_name),
         }
     }
 }
@@ -55,9 +54,11 @@ impl fmt::Display for Error {
 impl fmt::Display for Plugin {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut basedirs = HashSet::new();
-        writeln!(f, "# {}", self.name)?;
+        writeln!(f, "# {}/{}", self.author, self.name)?;
         for file in &self.files {
-            basedirs.insert(file.parent().unwrap());
+            if let Some(basedir) = file.parent() {
+                basedirs.insert(basedir);
+            }
             writeln!(f, "source {}", file.display())?;
         }
         for basedir in basedirs {
@@ -68,60 +69,72 @@ impl fmt::Display for Plugin {
     }
 }
 
-impl Plugin {
+fn split(plugin_name: &str) -> Result<(String, String), Error> {
+    let plugin_path = PathBuf::from(plugin_name);
+    if plugin_path.components().count() != 2 {
+        return Err(Error::InvalidPluginName { plugin_name: plugin_name.to_string() })
+    }
 
-    pub fn from_name(zr_init: &Path, name: &str) -> Plugin {
-        let path = zr_init.parent().unwrap().join("plugins").join(&name);
+    let name = plugin_path.components().last().unwrap().as_os_str().to_str().unwrap();
+    let author = plugin_path.parent().unwrap().components().last().unwrap().as_os_str().to_str().unwrap();
+
+    Ok((author.to_string(), name.to_string()))
+}
+
+impl Plugin {
+    pub fn from_plugin_name(zr_init: &Path, plugin_name: &str) -> Result<Plugin, Error> {
+        let (author, name) = split(plugin_name)?;
+        Plugin::new(&zr_init, &author, &name)
+    }
+
+    pub fn new(zr_init: &Path, author: &str, name: &str) -> Result<Plugin, Error> {
+        let path = zr_init.parent().unwrap().join("plugins").join(&author).join(&name);
         if ! path.exists() {
-            let url = format!("https://github.com/{}", name);
+            fs::create_dir(path.parent().unwrap()).expect("Had trouble creating a directory");
+            let url = format!("https://github.com/{}/{}", author, name);
             let _ = match Repository::clone(&url, &path) {
                 Ok(repo) => repo,
                 Err(e) => panic!("failed to clone: {}", e),
             };
         }
-        Plugin::from_path(&path)
-    }
 
-    pub fn from_files(name: &Path, files: Vec<PathBuf>) -> Plugin {
-        Plugin {
-            name: name.to_str().unwrap().to_string(),
-            files: files.iter().cloned().collect(),
-        }
-    }
-
-    pub fn from_path(path: &Path) -> Plugin {
-        let name = path.parent().unwrap();
-
-        let files: Vec<_> = path.read_dir().unwrap()
+        let files: Vec<PathBuf> = path.read_dir().unwrap()
             .filter_map(std::result::Result::ok)
             .map(|file| file.path())
             .filter(|file| file.is_file() && file.extension().is_some())
             .collect();
 
-        // antigen style
-        if let Some(antigen_plugin_file) = files.iter().find(|&file| *file == path.join(name).with_extension("plugin.zsh")) {
-            return Plugin::from_files(name, vec![antigen_plugin_file.to_owned()]);
-        }
+        let sources: Vec<PathBuf> = {
+            if let Some(antigen_plugin_file) = files.iter().find(|&file| *file == path.join(&name).with_extension("plugin.zsh")) {
+                vec![antigen_plugin_file.to_owned()]
+            } else if let Some(prezto_plugin_file) = files.iter().find(|&file| *file == path.join("init.zsh")) {
+                vec![prezto_plugin_file.to_owned()]
+            } else {
+                let zsh_plugin_files: Vec<_> = files.iter().cloned().filter(|ref file| file.extension() == Some(OsStr::new("zsh"))).collect();
+                let shell_files = if zsh_plugin_files.is_empty() {
+                    files.iter().cloned().filter(|file| file.extension().unwrap() == "sh").collect()
+                } else {
+                    zsh_plugin_files
+                };
+                shell_files
+            }
 
-        // prezto style
-        if let Some(prezto_plugin_file) = files.iter().find(|&file| *file == path.join("init.zsh")) {
-            return Plugin::from_files(name, vec![prezto_plugin_file.to_owned()]);
-        }
+        };
 
-        // zsh plugins
-        let zsh_plugin_files: Vec<_> = files.iter().cloned().filter(|ref file| file.extension() == Some(OsStr::new("zsh"))).collect();
-        if ! zsh_plugin_files.is_empty() {
-            return Plugin::from_files(name, zsh_plugin_files);
-        }
-
-        // sh plugins
-        let sh_plugin_files: Vec<_> = files.iter().cloned().filter(|file| file.extension().unwrap() == "sh").collect();
-        if ! sh_plugin_files.is_empty() {
-            return Plugin::from_files(name, sh_plugin_files);
-        }
-
-        Plugin::from_files(name, vec![])
+        Ok(Plugin { author: author.to_string(), name: name.to_string(), files: HashSet::from_iter(sources) } )
     }
+
+    pub fn from_files(zr_init: &Path, author: &str, name: &str, files: Vec<PathBuf>) -> Plugin {
+        let path = zr_init.parent().unwrap().join("plugins").join(&author).join(&name);
+        let mapped = files.iter().cloned().map(|file| path.join(&file)).collect();
+
+        Plugin {
+            author: author.to_string(),
+            name: name.to_string(),
+            files: mapped,
+        }
+    }
+
 }
 
 fn main() {
@@ -173,11 +186,12 @@ fn run() -> Result<(), Error> {
 
     match zr.clone().get_matches().subcommand() {
         ("add", Some(matches)) => {
-            let plugin = matches.value_of("plugin").expect("add should have a plugin specified");
-            if matches.is_present("file") {
-                add(&zr_init, plugin, matches.value_of("file").unwrap())
+            let plugin_name = matches.value_of("plugin").expect("add should have a plugin specified");
+            let (author, name) = split(&plugin_name)?;
+            if let Some(file) = matches.value_of("file") {
+                add_file(&zr_init, author, name, file)?;
             } else {
-                add(&zr_init, plugin, "")
+                add(&zr_init, author, name)?;
             }
         },
         ("list", _) => list(&zr_init),
@@ -198,7 +212,7 @@ fn reset(zr_init: &Path) {
 
 fn list(zr_init: &Path) {
     for plugin in plugins(zr_init) {
-        println!("{}", plugin.name);
+        println!("{}/{}", plugin.author, plugin.name);
     }
 }
 
@@ -214,44 +228,42 @@ fn plugins(zr_init: &Path) -> Vec<Plugin> {
         .map(|line| line.unwrap())
         .filter(|line| line.starts_with('#'))
         .map(|line| line.split_whitespace().last().unwrap().to_owned())
-        .map(|plugin_name| Plugin::from_name(zr_init, &plugin_name))
+        .map(|plugin_name| Plugin::from_plugin_name(&zr_init, &plugin_name).ok().unwrap())
         .collect::<Vec<Plugin>>()
 }
 
-fn add(zr_init: &Path, name: &str, file: &str) {
-    let plugins = load(zr_init, name, file);
-    save(zr_init, plugins);
-}
-
-fn load(zr_init: &Path, name: &str, file: &str) -> Vec<Plugin> {
+fn add_file(zr_init: &Path, author: String, name: String, file: &str) -> Result<(), Error> {
     let mut plugins = plugins(zr_init);
-
-    let plugin_exists = plugins.iter().any(|plugin| plugin.name == name);
-    let has_filename = file != "";
-
-    if has_filename {
-        if plugin_exists {
-            plugins.iter_mut().find(|plugin| plugin.name == name).unwrap().files.insert(PathBuf::from(&file));
-        } else {
-            plugins.push(Plugin::from_files(Path::new(name), vec![PathBuf::from(&file)]));
-        }
-    } else if !plugin_exists {
-        plugins.push(Plugin::from_name(zr_init, name));
+    if plugins.iter().find(|ref plugin| (&plugin.name, &plugin.author) == (&name, &author)).is_none() {
+        plugins.push(Plugin::from_files(&zr_init, &author, &name, vec![PathBuf::from(&file)]));
+    } else if let Some(plugin) = plugins.iter_mut().find(|ref plugin| (&plugin.name, &plugin.author) == (&name, &author)) {
+        plugin.files.insert(zr_init.parent().unwrap().join("plugins").join(&author).join(&name).join(&file));
     }
 
-    plugins
+    save(zr_init, plugins)
 }
 
-fn save(zr_init: &Path, plugins: Vec<Plugin>) {
+fn add(zr_init: &Path, author: String, name: String) -> Result<(), Error> {
+    let mut plugins = plugins(zr_init);
+
+    if plugins.iter().all(|ref plugin| (&plugin.name, &plugin.author) != (&name, &author)) {
+        plugins.push(Plugin::new(&zr_init, &author, &name)?);
+    }
+
+    save(zr_init, plugins)
+}
+
+fn save(zr_init: &Path, plugins: Vec<Plugin>) -> Result<(), Error> {
     let temp_filename = format!("{}init.zsh", std::env::temp_dir().display());
     let mut temp_file = OpenOptions::new().write(true).create_new(true).open(&temp_filename).unwrap();
 
     for plugin in plugins {
         writeln!(temp_file, "{}", plugin)
-            .expect("Should be able to write plugins");
+            .expect("Should be able to write to temp_file");
     }
     writeln!(temp_file, "autoload -Uz compinit; compinit -iCd $HOME/.zcompdump")
         .expect("Should be able to write the autoload line");
 
     fs::rename(&temp_filename, &zr_init).unwrap();
+    Ok(())
 }
