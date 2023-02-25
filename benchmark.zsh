731587ca28b10bcfc4a9294d7f55650fc18c9fff
Jonathan Dahan <git@jonathan.is>
2023-02-25 13:37:08 -0500

    Allow injecting binary for benchmarks

--- benchmark.zsh
+++ benchmark.zsh
@@ -1,6 +1,6 @@
 # hyperfine --warmup 3 'zsh -d -f -l -c "source $PWD/benchmark.zsh && zrinit && exit"'
 function zrinit {
-  XDG_CACHE_HOME=/tmp/zrbench ./target/release/zr \
+  XDG_CACHE_HOME=/tmp/zrbench ${1:-target/release/zr} \
     sorin-ionescu/prezto.git/modules/git/alias.zsh \
     sorin-ionescu/prezto.git/modules/history/init.zsh \
     junegunn/fzf.git/shell/key-bindings.zsh \
